e3c505d42bc60037f06a3b2a1f621d5ef44138e1
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2026-02-16 08:14:53 -0500

    fix: resolve ngx-translate deprecation and NG0908 error
    - Replace deprecated defaultLanguage with fallbackLang
    - Use standalone provideTranslateService() + provideTranslateHttpLoader()
    - Remove redundant importProvidersFrom(TranslateModule.forRoot()) that caused NG0908

--- hospital-portal/src/app/app.config.ts
+++ hospital-portal/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
@@ -12,11 +12,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
+    provideTranslateService({
+      fallbackLang: 'en',
+    }),
     provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'en',
-      }),
-    ),
   ],
 };
